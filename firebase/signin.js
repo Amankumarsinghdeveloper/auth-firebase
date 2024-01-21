91c9c9dcf22156b3bbe56e32d701d21cc1750721
Aman Kumar Singh <54640300+Amankumarsinghdeveloper@users.noreply.github.com>
2024-01-21 16:49:26 +0530

    update

--- firebase/signin.js
+++ firebase/signin.js
@@ -9,6 +9,7 @@ export default async function signIn(email, password) {
   try {
     result = await signInWithEmailAndPassword(auth, email, password);
   } catch (e) {
+    alert(e.message);
     error = e;
   }
 
